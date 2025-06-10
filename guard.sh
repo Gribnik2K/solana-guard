17de526844d737cf07b42a765d1fbfb034d134c9
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-06-10 21:37:01 +0300

    Update guard.sh
    добавил  -vvv в функцию ssh # расширенный отладочный вывод

--- guard.sh
+++ guard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GUARD_VER=v1.8.9
+GUARD_VER=v1.8.10
 #=================== guard.cfg ========================
 PORT='22' # remote server ssh port
 KEYS=$HOME/keys
@@ -277,10 +277,11 @@ SSH(){
 	local err_file="/tmp/ssh_error.tmp"
 	trap 'rm -f "$err_file"' EXIT # Trap для удаления временного файла при выходе
 
-  	command_output=$(timeout 5 ssh -o ConnectTimeout=4 REMOTE $ssh_command 2>$err_file)
+  	command_output=$(timeout 5 ssh -vvv -o ConnectTimeout=4 REMOTE $ssh_command 2>$err_file)
 	command_exit_status=$?
  	# timeout - ограничивает общее время выполнения ssh-команды (попытка соединения + выполнение команды)
   	# ConnectTimeout - допустимое время на установления TCP-соединения
+   	# -vvv - расширенный отладочный вывод
 		
 	# Диагностика SSH ошибок
   	if [ $command_exit_status -ne 0 ]; then
